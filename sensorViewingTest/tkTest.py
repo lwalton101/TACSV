44156f103c3308d5bfbf84afbaa007cbf11b7df6
lwalton101 <waltonfamily01@outlook.com>
2022-12-12 17:30:47 +0000

    Cleaned up tkTest.py

--- sensorViewingTest/tkTest.py
+++ sensorViewingTest/tkTest.py
@@ -1,12 +1,7 @@
-import tkinter as tk
+import tkinter as tk, serial, ctypes, matplotlib.pyplot as plt, matplotlib.image as mpimg
 from tkinter import *
-from tkinter import ttk
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from tkinter import ttk 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import json
-import ctypes
-import serial
 from RepeatingTimer import RepeatingTimer
 
 device_COM = 'COM3'
@@ -16,18 +11,20 @@ myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 window = tk.Tk()
+
 graph = None
 figure = None
+
 yLimEntry = None
 xLim = (0, 100)
 yLim = (0, 100)
-dataFile = json.load(open("Data.json", "r"))
+
+time = []
 temps = []
 pressures = []
 temp = IntVar()
 pressure = IntVar()
 goblinMode = IntVar()
-time = []
 
 def getData():
     response = str(device.read_all(), 'utf-8')
@@ -70,13 +67,11 @@ def refreshGraph():
     ax.legend()
 
     
-    graph = FigureCanvasTkAgg(figure, tab2)
+    graph = FigureCanvasTkAgg(figure, graphTab)
     graph.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH)
     split = yLimEntry.get().split(",")
     ax.set_ylim(float(split[0]), float(split[1]))
     ax.set_xlabel('Time')
-
-
 
 window.geometry("1000x800")
 window.wm_resizable(False, False)
@@ -87,25 +82,23 @@ window.protocol("WM_DELETE_WINDOW", quit_me)
 my_notebook= ttk.Notebook(window)
 my_notebook.pack(expand=1,fill=BOTH)
 
-tab1= ttk.Frame(my_notebook)
-my_notebook.add(tab1, text= "Home")
-tab2= ttk.Frame(my_notebook)
-my_notebook.add(tab2, text= "Graphs")
-#Create a Label in Tabs
-Label(tab1, text= "Tab 1", font= ('Helvetica 20 bold')).pack()
-Label(tab2, text= "Tab 2", font=('Helvetica 20 bold')).pack()
+homeTab= ttk.Frame(my_notebook)
+my_notebook.add(homeTab, text= "Home")
+graphTab = ttk.Frame(my_notebook)
+my_notebook.add(graphTab, text= "Graphs")
 
-Checkbutton(tab2, text="Temperature", variable=temp, onvalue=1, offvalue=0).pack(side=tk.BOTTOM)
-Checkbutton(tab2, text="Pressure", variable=pressure, onvalue=1, offvalue=0).pack(side=tk.BOTTOM)
-Checkbutton(tab2, text="Goblin Mode", variable=goblinMode, onvalue=1, offvalue=0).pack(side=tk.BOTTOM)
+Label(homeTab, text= "Home", font= ('Helvetica 20 bold')).pack()
+Label(graphTab, text= "Graphs", font=('Helvetica 20 bold')).pack()
 
-yLimEntry=Entry(tab2, width=35)
+Checkbutton(graphTab, text="Temperature", variable=temp, onvalue=1, offvalue=0).pack(side=tk.BOTTOM)
+Checkbutton(graphTab, text="Pressure", variable=pressure, onvalue=1, offvalue=0).pack(side=tk.BOTTOM)
+Checkbutton(graphTab, text="Goblin Mode", variable=goblinMode, onvalue=1, offvalue=0).pack(side=tk.BOTTOM)
+
+yLimEntry=Entry(graphTab, width=35)
 yLimEntry.insert(0, "0,100")
 yLimEntry.pack(side=tk.BOTTOM)
-Button(tab2, text="Refresh", command=refreshGraph).pack(side=tk.BOTTOM)
+Button(graphTab, text="Refresh", command=refreshGraph).pack(side=tk.BOTTOM)
 
 refreshGraph()
-
-
 
 window.mainloop()
