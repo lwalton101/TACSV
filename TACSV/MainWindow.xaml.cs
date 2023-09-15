3dfee659df21d523103d79e415dc32ca517b74f9
lwalton101 <waltonfamily01@outlook.com>
2023-09-15 20:22:22 +0100

    switching pages

--- TACSV/MainWindow.xaml.cs
+++ TACSV/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using ModernWpf.Controls;
 
 namespace TACSV
 {
@@ -19,6 +21,27 @@ namespace TACSV
 		{
             Trace.WriteLine(((Button)e.OriginalSource).Content.ToString());
             number++;
+		}
+
+		private void ListView_Selected(object sender, RoutedEventArgs e)
+		{
+            Trace.WriteLine("COCK N BALLS");
+            if (e.OriginalSource == null | MainPanel == null)
+            {
+                return;
+            }
+            var listView = e.OriginalSource as ModernWpf.Controls.ListViewItem;
+            MainPanel.Navigate(new Uri($"{listView.Content}.xaml", UriKind.Relative));
+        }
+
+		private void ListView_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			Trace.WriteLine("COCK N BALLS");
+		}
+
+		private void MainPanel_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+		{
+            MainPanel.NavigationService.RemoveBackEntry();
 		}
 	}
 }
