aeb9bb43f3624c122d6ba0d04584f90193012fe3
lwalton101 <waltonfamily01@outlook.com>
2023-09-15 19:15:09 +0100

    frame

--- TACSV/MainWindow.xaml.cs
+++ TACSV/MainWindow.xaml.cs
@@ -20,33 +20,5 @@ namespace TACSV
             Trace.WriteLine(((Button)e.OriginalSource).Content.ToString());
             number++;
 		}
-
-		private void CloseButton_Click(object sender, RoutedEventArgs e)
-		{
-            Close();
-		}
-
-		private void MaximiseButton_Click(object sender, RoutedEventArgs e)
-		{
-			MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-			MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
-			if (this.WindowState == WindowState.Maximized)
-				this.WindowState = WindowState.Normal;
-			else
-				this.WindowState = WindowState.Maximized;
-		}
-
-		private void MinimiseButton_Click(object sender, RoutedEventArgs e)
-		{
-			this.WindowState = WindowState.Minimized;
-		}
-
-		private void TitleBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-			if(e.ChangedButton == System.Windows.Input.MouseButton.Left)
-			{
-				this.DragMove();
-			}
-		}
 	}
 }
