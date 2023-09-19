6d8910c02daf6ec482ae25bd7c2f461a86d84933
lwalton101 <waltonfamily01@outlook.com>
2023-09-19 21:14:51 +0100

    final change which are wierd

---
+++ TACSV/PiPicoManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TACSV
+{
+    class PiPicoManager
+    {
+        private static SerialPort _port;
+
+        public static void ConnectToPort(string portName)
+        {
+            if (!portName.StartsWith("COM"))
+            {
+                MessageBox.Show($"Cannot connect to port {portName} that doesn't begin with COM");
+            }
+
+            _port = new SerialPort();
+            _port.PortName = portName;
+            _port.BaudRate = 9600;
+        }
+    }
+}
