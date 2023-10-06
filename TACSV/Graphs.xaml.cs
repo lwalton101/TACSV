c9697903e4cb8bcee334bf9dbc070d7d6ba6266a
lwalton101 <waltonfamily01@outlook.com>
2023-10-06 19:03:46 +0100

    added refresh time box

--- TACSV/Graphs.xaml.cs
+++ TACSV/Graphs.xaml.cs
@@ -1,19 +1,11 @@
-﻿using ScottPlot;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TACSV
 {
@@ -24,21 +16,55 @@ namespace TACSV
     {
 
         Random r = new Random();
+		List<double> dataX = new List<double>();
+		List<double> dataY = new List<double>();
+
         public Graphs()
         {
             InitializeComponent();
-            
-        }
+			dataX.Add(0);
+			dataY.Add(0);
+		}
 
 		private void Page_Loaded(object sender, RoutedEventArgs e)
 		{
-			double[] dataX = new double[] { 1, 2, 3, 4, 5 };
-			double[] dataY = new double[] { 1, 4, 9, 16, 25 };
-
 			PlotControl.Plot.Add.Scatter(dataX, dataY);
+			PlotControl.Plot.AutoScale();
 			PlotControl.Refresh();
+		}
 
-            Trace.WriteLine("COCK");
+		private void AutoScaleButton_Click(object sender, RoutedEventArgs e)
+		{
+			PlotControl.Plot.AutoScale();
+			PlotControl.Refresh();
+		}
+
+		private void RefreshButton_Click(object sender, RoutedEventArgs e)
+		{
+			PlotControl.Refresh();
+		}
+
+		private void RefreshTimeTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			Regex regex = new Regex("[0-9]");
+			e.Handled = !regex.IsMatch(e.Text);
+		}
+
+		private void RefreshTimeTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+		{
+			Regex regex = new Regex("[0-9]");
+			if (e.DataObject.GetDataPresent(typeof(String)))
+			{
+				String text = (String)e.DataObject.GetData(typeof(String));
+				if (!regex.IsMatch(text))
+				{
+					e.CancelCommand();
+				}
+			}
+			else
+			{
+				e.CancelCommand();
+			}
 		}
 	}
 }
