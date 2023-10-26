efa7cc861a04afde4b191f52a9df260c84765914
lwalton101 <waltonfamily01@outlook.com>
2023-10-26 22:06:40 +0100

    added the Tacsv Ground class

---
+++ TACSV/TACSVGround.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace TACSV;
+
+public class TACSVGround
+{
+    private SerialPort _port;
+    private Thread _thread;
+    public event EventHandler<string> OnMessageRecieved;
+    private string lineBeingRead = "";
+    
+    public string ComPort
+    {
+        get => _port.PortName;
+        set => _port.PortName = value;
+    }
+
+    public int BaudRate
+    {
+        get => _port.BaudRate;
+        set => _port.BaudRate = value;
+    }
+    
+    public int ReadTimeout
+    {
+        get => _port.ReadTimeout;
+        set => _port.ReadTimeout = value;
+    }
+
+    public TACSVGround()
+    {
+        _port = new SerialPort();
+        _port.DtrEnable = true;
+        _port.DataReceived += OnDataRecieved;
+    }
+
+    private void OnDataRecieved(object sender, SerialDataReceivedEventArgs e)
+    {
+        lineBeingRead += _port.ReadExisting();
+        if (lineBeingRead.EndsWith("\n"))
+        {
+            Console.WriteLine($"We got a line ender: {lineBeingRead}");
+            OnMessageRecieved.Invoke(this, lineBeingRead);
+            lineBeingRead = "";
+        }
+    }
+
+    public void Connect()
+    {
+        _port.Open();
+    }
+
+}
