cf54408f6233f6ebe8c36a482547940af0023594
lwalton101 <waltonfamily01@outlook.com>
2024-02-14 15:48:32 +0000

    AppDataPath and ConfigPath are now private

--- TACSV/Program.cs
+++ TACSV/Program.cs
@@ -7,8 +7,8 @@ namespace TACSV
 	public static class Program
 	{
 		public static TACSVGround Ground = new();
-		public static string AppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TACSV");
-		public static readonly string ConfigPath = Path.Combine(AppDataPath, "TACSVConfig.json");
+		private static string AppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TACSV");
+		private static readonly string ConfigPath = Path.Combine(AppDataPath, "TACSVConfig.json");
 		
 		public static readonly IApplicationOptions Options = new ConfigurationBuilder<IApplicationOptions>()
 			.UseJsonFile(ConfigPath)
